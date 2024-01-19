929efdc80af13805679ddc267cefeb292237deee
rajcantcode <mahadeshwarraj23@gmail.com>
2024-01-19 22:56:17 +0530

    remove unused imports

--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect, useRef } from "react";
 import { useSelector } from "react-redux";
-import YouTube from "react-youtube";
 
 import "../stylesheets/videoPlayer.css";
 // Base styles for media player and provider (~400B).
