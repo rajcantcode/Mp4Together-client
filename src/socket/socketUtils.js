4ee0574ab529466c3acf406fa762f8127335e9ce
rajcantcode <mahadeshwarraj23@gmail.com>
2024-01-21 10:43:46 +0530

    Add auth and refactor to prevent multiple socket connections

--- src/socket/socketUtils.js
+++ src/socket/socketUtils.js
@@ -1,9 +1,25 @@
-import { useMemo } from "react";
 import io from "socket.io-client";
 
 const baseUrl = import.meta.env.VITE_BACKEND_URL;
 
-export const socket = io(baseUrl);
+const socketObj = {
+  socket: null,
+};
+
+export const getSocket = () => {
+  if (socketObj.socket) {
+    return socketObj.socket;
+  }
+  const socket = io(baseUrl, {
+    withCredentials: true,
+  });
+  socketObj.socket = socket;
+  socket.on("disconnect", (reason) => {
+    socketObj.socket = null;
+    console.log("Socket disconnected");
+  });
+  return socket;
+};
 
 export const joinSocketRoom = (room, socket, username) => {
   socket.emit("join", { room, username });
