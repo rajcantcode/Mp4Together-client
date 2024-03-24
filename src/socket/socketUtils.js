e582ebd7d8c1249dabb194725c7f35a6c53f6db9
rajcantcode <mahadeshwarraj23@gmail.com>
2024-03-24 23:47:02 +0530

    Add SFU socket functionality

--- src/socket/socketUtils.js
+++ src/socket/socketUtils.js
@@ -1,6 +1,7 @@
 import io from "socket.io-client";
 
 const baseUrl = import.meta.env.VITE_BACKEND_URL;
+const sfuServerUrl = import.meta.env.VITE_SFU_SERVER_URL;
 
 const socketObj = {
   socket: null,
@@ -19,6 +20,30 @@ export const getSocket = () => {
     console.log("Socket disconnected");
   });
   return socket;
+};
+
+const sfuSocketObj = {
+  socket: null,
+};
+
+export const getSfuSocket = () => {
+  if (sfuSocketObj.socket) {
+    return sfuSocketObj.socket;
+  }
+  const sfuSocket = io(sfuServerUrl, { withCredentials: true });
+  sfuSocketObj.socket = sfuSocket;
+  sfuSocket.on("connect", () => {
+    console.log("sfu socket connected, " + sfuSocket.id);
+  });
+  sfuSocket.on("disconnect", (reason) => {
+    sfuSocketObj.socket = null;
+    console.log("Sfu socket disconnected");
+  });
+  sfuSocket.on("close", (reason) => {
+    console.log("sfuSocket closed");
+    console.log(reason);
+  });
+  return sfuSocket;
 };
 
 export const joinSocketRoom = (room, socket, username) => {
