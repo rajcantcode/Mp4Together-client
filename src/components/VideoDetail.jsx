abb17bc6531ca6e39d46038ba26a67c3f876726d
rajcantcode <mahadeshwarraj23@gmail.com>
2024-05-30 17:17:28 +0530

    refactor: Adjust VideoDetail component layout and styling

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -4,16 +4,20 @@ const VideoDetail = ({ videoData, onClick }) => {
       className="flex w-full h-[100px] cursor-pointer border-b-2 border-gray-300"
       onClick={onClick}
     >
-      <div className="thumbnail min-w-[20%] flex items-center">
+      <div className="thumbnail min-w-[30%] max-w-[30%] flex items-center ">
         <img
           src={videoData.thumbnail}
           alt={videoData.name}
           className="block mx-auto"
         />
       </div>
-      <div className="info">
-        <p className="text-xl text-black-500">{videoData.name.trim()}</p>
-        <p className="text-base text-gray-400">{videoData.channel.trim()}</p>
+      <div className="info pl-[10px] flex flex-col justify-center">
+        <p className="text-sm md:text-lg text-black-500">
+          {videoData.name.trim()}
+        </p>
+        <p className="text-xs text-gray-400 md:text-base">
+          {videoData.channel.trim()}
+        </p>
       </div>
     </div>
   );
