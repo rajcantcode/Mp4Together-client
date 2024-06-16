c7c63f72bb2587935c23718a36b3a1363b19e1c4
rajcantcode <mahadeshwarraj23@gmail.com>
2024-06-16 20:00:42 +0530

    refactor: Add max-xs screen size to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      screens: {
+        "max-xs": { max: "400px" },
+      },
       keyframes: {
         "caret-blink": {
           "0%,70%,100%": { opacity: "1" },
