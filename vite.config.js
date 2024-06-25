c4d737e132565af05133b92d39c45ca00ae1f512
rajcantcode <mahadeshwarraj23@gmail.com>
2024-06-25 15:31:02 +0530

    feat: customize build

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,43 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { visualizer } from "rollup-plugin-visualizer";
+import { dependencies } from "./package.json";
+
+function renderChunks(deps) {
+  let chunks = {};
+  Object.keys(deps).forEach((key) => {
+    if (
+      [
+        "react",
+        "react-router-dom",
+        "react-dom",
+        "@chatscope/chat-ui-kit-styles",
+        "axios",
+      ].includes(key)
+    )
+      return;
+    chunks[key] = [key];
+  });
+  return chunks;
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    visualizer({ open: true, filename: "bundle-analysis.html" }),
+  ],
+  build: {
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ["react", "react-router-dom", "react-dom", "axios"],
+          ...renderChunks(dependencies),
+        },
+      },
+    },
+  },
   server: {
     port: 5173,
   },
